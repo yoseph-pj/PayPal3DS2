94e7608e2be1dac12b12bb43c9b719a737309fa6
Yoseph <yhtilahun@yahoo.com>
2023-01-20 15:26:00 -0800

    Update Package.swift
    Updated Platform.

--- Package.swift
+++ Package.swift
@@ -5,6 +5,9 @@ import PackageDescription
 
 let package = Package(
     name: "PayPal3DS2",
+    platforms: [
+            .iOS(.v15)         //.v15
+    ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
